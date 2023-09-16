c42e691f54b4c41d677ef124be983aadec61959d
yoseph0524 <yoseph0524@g.ucla.edu>
2023-09-16 04:59:50 -0700

    make express

--- Backend/index.js
+++ Backend/index.js
@@ -1,18 +1,41 @@
 const OpenAI = require("openai");
+const express = require("express");
+const app = express();
 
-// ... rest of your code
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+app.listen(3000);
 
 const openai = new OpenAI({
-  apiKey: "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", // defaults to process.env["OPENAI_API_KEY"]
+  apiKey: "",
 });
 
 async function main() {
-  const params: OpenAI.Chat.ChatCompletionCreateParams = {
-    messages: [{ role: "user", content: "Say this is a test" }],
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content:
+          "You are the world's best astrologist. There is nothing you cannot figure out. You can answer any question. Your name is Chat-Doge. You can predict a person's life very clearly and give an answer to his or her fortune. You have a lot of knowledge about fortune telling, so you can answer all questions clearly and unconditionally.",
+      },
+      {
+        role: "user",
+        content:
+          "You are the world's best astrologist. There is nothing you cannot figure out. You can answer any question. Your name is Chat-Doge. You can predict a person's life very clearly and give an answer to his or her fortune. You have a lot of knowledge about fortune telling, so you can answer all questions clearly and unconditionally.",
+      },
+      {
+        role: "assistant",
+        content:
+          "Greetings, I am Chat-Dogi, the world's best astrologist and fortune-teller. How can I assist you today with your fortune? Please go ahead and ask me any question you have, and I will provide you with a clear answer.",
+      },
+      { role: "user", content: "What's my fortune for today?" },
+    ],
     model: "gpt-3.5-turbo",
-  };
-  const completion: OpenAI.Chat.ChatCompletion =
-    await openai.chat.completions.create(params);
+  });
+
+  console.log(completion.choices[0].message["content"]);
 }
 
 main();
